be3c6a995898f0b285338048e8f77671da86f379
ranjit.chavan <er.ranjitchavan@gmail.COM>
2018-08-23 13:25:31 +0530

    DI using XML

---
+++ IntroToSpringAOP/src/com/spring/shape/Rectangle.java
@@ -0,0 +1,10 @@
+package com.spring.shape;
+
+public class Rectangle implements Shape {
+
+	public void draw() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ IntroToSpringAOP/src/com/spring/shape/Shape.java
@@ -0,0 +1,7 @@
+package com.spring.shape;
+
+public interface Shape {
+	
+	public void draw();
+	
+}
